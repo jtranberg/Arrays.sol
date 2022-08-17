781ee25d28bef15203270c91c5196039e4f1f516
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-08-16 20:50:40 -0700

    arrays in solidity

---
+++ Arrays.sol
@@ -0,0 +1,20 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+//[] array..lists of...colors, adressess, same items only
+contract Arrays {
+
+    uint[] public myArray;
+    uint[] public myArray2;
+    uint[200] public myFixedSizeArray;
+
+    function push(uint number) public {
+         myArray.push(number);
+    }
+
+    function pop() public {
+        myArray.pop();
+    }
+
+    function getlenght() public view returns (uint) {
+        myArray.lenght;
+    }
+}
